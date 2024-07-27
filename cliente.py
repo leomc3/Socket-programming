8423799cc83768da8fa39f9d923b1451e0b2082a
Leeomc <leonardomc3@hotmail.com>
2024-07-26 23:48:06 -0300

    Update cliente.py

--- cliente.py
+++ cliente.py
@@ -2,18 +2,15 @@ import socket
 import ssl
 import threading
 
-# Caminhos para os arquivos de certificado e chave privada
 CERT_FILE = "client_certificate.pem"
 KEY_FILE = "client_key.pem"
-SERVER_CERT_FILE = "server_certificate.pem"
+SERVER_CERT_FILE = "ca_certificate.pem"
 
-# Função para criar o contexto SSL
 def create_ssl_context():
     context = ssl.create_default_context(ssl.Purpose.SERVER_AUTH, cafile=SERVER_CERT_FILE)
     context.load_cert_chain(certfile=CERT_FILE, keyfile=KEY_FILE)
     return context
 
-# Função para receber mensagens do servidor
 def receive_messages(sock):
     while True:
         try:
@@ -26,33 +23,21 @@ def receive_messages(sock):
             print(f"Erro ao receber mensagem: {str(e)}")
             break
 
-# Função principal do cliente
 def main():
-    # Cria o contexto SSL
     context = create_ssl_context()
-    
-    # Conecta ao servidor
-    server_address = ('localhost', 1234)
+    server_address = ('20.186.15.152', 1234)
     with socket.create_connection(server_address) as sock:
-        with context.wrap_socket(sock, server_hostname='localhost') as ssock:
+        with context.wrap_socket(sock, server_hostname='20.186.15.152') as ssock:
             print("Conectado ao servidor")
-
-            # Inicia uma thread para receber mensagens do servidor
             threading.Thread(target=receive_messages, args=(ssock,)).start()
-
-            # Aguarda o comando LOGIN
             while True:
                 mensagem = input()
-                if mensagem.startswith("LOGIN"):
+                if mensagem.startswith("LOGIN") or mensagem.startswith("LOGOFF") or mensagem.startswith("MSG") or mensagem.startswith("CREATE-USER"):
                     ssock.sendall(mensagem.encode('utf-8'))
-                elif mensagem.startswith("LOGOFF"):
-                    ssock.sendall(mensagem.encode('utf-8'))
-                    break
-                elif mensagem.startswith("MSG"):
-                    ssock.sendall(mensagem.encode('utf-8'))
+                    if mensagem.startswith("LOGOFF"):
+                        break
                 else:
-                    print("Comando não reconhecido. Use: LOGIN username password, LOGOFF username, MSG client-id text ou MSG ALL text")
-            
+                    print("Comando não reconhecido. Use: LOGIN username password, LOGOFF username, MSG client-id text, MSG ALL text ou CREATE-USER username password")
             print("Desconectado do servidor")
 
 if __name__ == "__main__":
